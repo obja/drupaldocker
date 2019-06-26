4a5a04396a4d528d1b2f8c6168a878dfdd537db5
mattyoun <mattyoun@qualcomm.com>
2019-06-26 09:04:43 -0700

    added env vars

--- drupal/web/sites/default/settings.docker.php
+++ drupal/web/sites/default/settings.docker.php
@@ -1,12 +1,11 @@
 <?php
 $databases['default']['default'] = array(
-  'database' => 'drupaldb',
-  'username' => 'drupal',
-  'password' => 'verybadpassword',
+  'database' => getenv('MYSQL_DATABASE'),
+  'username' => getenv('MYSQL_USER'),
+  'password' => getenv('MYSQL_PASSWORD'),
   'prefix' => '',
   'host' => 'db',
   'port' => '3306',
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
 );
-$config_directories['sync'] = 'sites/default/files/config_41yJEESSQlH9ovXS1IJz-fwm3O58HN1SMKP_nhZq7O0JlD8SW9OGl3f9QFpp9OxBtJ0lSDPrJA/sync';
