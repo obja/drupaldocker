8caa7685fec70c5548735d0c0e41c26bd594dc74
mattyoun <mattyoun@qualcomm.com>
2019-06-26 10:11:05 -0700

    multisite docker

---
+++ docroot/sites/myothersite.test/settings.docker.php
@@ -0,0 +1,11 @@
+<?php
+$databases['default']['default'] = array(
+  'database' => getenv('MYSQL_DATABASE'),
+  'username' => getenv('MYSQL_USER'),
+  'password' => getenv('MYSQL_PASSWORD'),
+  'prefix' => '',
+  'host' => 'db',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
